aee4c9411a4bfbd323d774e61f9ad82888bb67b5
Rowell Dionicio <rowelldionicio@gmail.com@users.noreply.github.com>
2020-05-10 09:09:37 -0700

    Add comment about API key

--- parsing/getOrganizations.py
+++ parsing/getOrganizations.py
@@ -6,7 +6,7 @@ url = "https://dashboard.meraki.com/api/v0/organizations"
 payload = {}
 headers = {
   'X-Cisco-Meraki-API-Key': '093b24e85df15a3e66f1fc359f4c48493eaa1b73'
-}
+} # Demo API key from Meraki
 
 response = requests.request("GET", url, headers=headers, data = payload)
 
