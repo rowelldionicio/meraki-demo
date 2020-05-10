b8c70d917511fd62006e6378b7e969f5130107a7
Rowell Dionicio <rowelldionicio@gmail.com@users.noreply.github.com>
2020-05-10 09:33:12 -0700

    getNetworks - initiating script. getOrganizations - added for loop

--- parsing/getNetworks.py
+++ parsing/getNetworks.py
@@ -1,16 +1,16 @@
 import requests
 import json
 
-url = "https://dashboard.meraki.com/api/v0/organizations"
+url = "https://dashboard.meraki.com/api/v0/organizations/537758/networks"
 
 payload = {}
 headers = {
   'X-Cisco-Meraki-API-Key': '093b24e85df15a3e66f1fc359f4c48493eaa1b73'
-} # Demo API key from Meraki
+}
 
 response = requests.request("GET", url, headers=headers, data = payload)
 
-r = json.loads(response.text)
+networks = json.loads(response.text)
 
-for organizations in r:
-    print(organizations['name'])
+for n in networks:
+    print('Network ID:', n['id'],'\t Name:', n['name'])
